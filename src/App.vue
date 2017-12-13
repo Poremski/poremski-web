a2d63130080aea21f90961717792ee0696645ad5
Javier Poremski <javier@poremski.se>
2017-12-13 17:48:21 +0100

    Add head container

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
   <div id="app">
+    <div class="preloader">
+      <div class="anim pulse"><i class="ion-ios-bolt-outline"></i></div>
+    </div>
+    <div class="preloader-left"></div>
+    <div class="inline-menu-container">
+      <span class="status contact menu-item">{{ $t('menu.status', 'Jag är tillgänglig för frilans')}}</span>
+      <ul class="inline-menu">
+        <li class="about menu-item">{{ $t('menu.items.resume', 'Resumé')}}</li>
+        <li class="portfolio menu-item">{{ $t('menu.items.portfolio', 'Portfolio')}}</li>
+        <li class="blog menu-item">{{ $t('menu.items.blog', 'Blogg')}}</li>
+        <li class="contact menu-item">{{ $t('menu.items.contact', 'Kontakt')}}</li>
+        <li id="close" class="menu-item"><i class="ion-ios-close-empty"></i></li>
+      </ul>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -9,14 +23,3 @@ export default {
   name: 'app'
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
