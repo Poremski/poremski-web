a3a966be1bd2d8abd21491e8e99ff1ec863d3d19
Javier Poremski <javier@poremski.se>
2017-12-13 17:50:04 +0100

    Add component for About, Blog, Contact and Portfolio

---
+++ src/components/About.vue
@@ -0,0 +1,202 @@
+<template>
+  <div class="content-blocks about">
+    <section class="content" id="about">
+      <div class="block-content">
+        <h3 class="block-title">Profile</h3>
+        <h3 class="subheading">I'm a <strong>UI/UX Designer & Frontend Developer</strong> from Helsingborg, Sweden. I aim to make a difference through my creative solution.</h3>
+        <p></p>
+        <div class="info-list row">
+          <div class="col-sm-6"><span>Name : </span> Javier Poremski</div>
+          <div class="col-sm-6"><span>Date of birth : </span> November 5, 1986</div>
+          <div class="col-sm-6"><span>Location : </span> Helsingborg, Scania, Sweden</div>
+          <div class="col-sm-6"><span>Email : </span> javier@poremski.se</div>
+          <div class="col-sm-6"><span>Phone : </span> +46 762 20 86 05</div>
+        </div>
+        <img class="sign" src="/static/img/signature.png" alt="">
+      </div>
+      <div class="block-content">
+        <h3 class="block-title">Skills</h3>
+        <label class="progress-bar-label">HTML & CSS - <span>10 years experience</span></label>
+        <div class="progress">
+          <div style="width: 99%;" class="progress-bar" role="progressbar" aria-valuenow="99" aria-valuemin="0" aria-valuemax="100">
+            <span>99%</span>
+          </div>
+        </div>
+        <label class="progress-bar-label">JavaScript - <span>5 years experience</span></label>
+        <div class="progress">
+          <div style="width: 80%;" class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+            <span>99%</span>
+          </div>
+        </div>
+        <label class="progress-bar-label">Java - <span>5 years experience</span></label>
+        <div class="progress">
+          <div style="width: 85%;" class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
+            <span>99%</span>
+          </div>
+        </div>
+        <label class="progress-bar-label">Android Development - <span>5 years experience</span></label>
+        <div class="progress">
+          <div style="width: 45%;" class="progress-bar" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100">
+            <span>99%</span>
+          </div>
+        </div>
+        <label class="progress-bar-label">Python - <span>5 years experience</span></label>
+        <div class="progress">
+          <div style="width: 90%;" class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+            <span>99%</span>
+          </div>
+        </div>
+        <label class="progress-bar-label">Swift - <span>2 years experience</span></label>
+        <div class="progress">
+          <div style="width: 40%;" class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100">
+            <span>99%</span>
+          </div>
+        </div>
+      </div>
+      <div class="block-content">
+        <h3 class="block-title">Experience</h3>
+        <div class="timeline experience">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="exp-holder">
+                <div class="exp">
+                  <div class="hgroup">
+                    <h4><span>Customer Service Co-Worker</span> @ IKEA Group</h4>
+                    <h5>Oct 2008 - <span class="current">Present</span></h5>
+                  </div>
+                  <p></p>
+                </div>
+                <div class="exp">
+                  <div class="hgroup">
+                    <h4><span>Sales & Supply Support Specialist</span> @ IKEA Group</h4>
+                    <h5>Nov 2008 - Aug 2015</h5>
+                  </div>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="block-content">
+        <h3 class="block-title">Education</h3>
+        <div class="timeline education">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="exp-holder">
+                <div class="exp">
+                  <div class="hgroup">
+                    <h4>B.Sc. in Computer Science | <span>Lund University, Sweden</span></h4>
+                    <h5>Aug 2016 - <span class="current">Present</span></h5>
+                  </div>
+                  <p></p>
+                </div>
+                <div class="exp">
+                  <div class="hgroup">
+                    <h4>B.Sc. in Biomedical Science | <span>Malmö University, Sweden</span></h4>
+                    <h5>Aug 2010 - Jun 2013</h5>
+                  </div>
+                  <p></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="block-content">
+        <h3 class="block-title">Services</h3>
+        <div class="row">
+          <div class="col-md-4 col-sm-6 service">
+            <div class="service-icon">
+              <i class="ion-laptop"></i>
+            </div>
+            <h4>Web Design</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+          </div>
+          <div class="col-md-4 col-sm-6 service">
+            <div class="service-icon">
+              <i class="ion-pie-graph"></i>
+            </div>
+            <h4>App Development</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+          </div>
+          <div class="col-md-4 col-sm-6 service">
+            <div class="service-icon">
+              <i class="ion-paintbucket"></i>
+            </div>
+            <h4>Graphic Design</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+          </div>
+          <div class="col-md-4 col-sm-6 service">
+            <div class="service-icon">
+              <i class="ion-printer"></i>
+            </div>
+            <h4>Print Design</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+          </div>
+          <div class="col-md-4 col-sm-6 service">
+            <div class="service-icon">
+              <i class="ion-qr-scanner"></i>
+            </div>
+            <h4>Graphic Design</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+          </div>
+          <div class="col-md-4 col-sm-6 service">
+            <div class="service-icon">
+              <i class="ion-social-buffer"></i>
+            </div>
+            <h4>Print Design</h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet.</p>
+          </div>
+        </div>
+      </div>
+      <div class="block-content">
+        <h3 class="block-title">Awards</h3>
+        <div class="row">
+          <div class="col-md-3 col-sm-6 awards">
+            <img src="/static/img/awards/1.jpg">
+          </div>
+          <div class="col-md-3 col-sm-6 awards">
+            <img src="/static/img/awards/2.jpg">
+          </div>
+          <div class="col-md-3 col-sm-6 awards">
+            <img src="/static/img/awards/3.jpg">
+          </div>
+          <div class="col-md-3 col-sm-6 awards">
+            <img src="/static/img/awards/4.jpg">
+          </div>
+        </div>
+      </div>
+      <div class="block-content">
+        <h3 class="block-title">Testimonials</h3>
+        <div class="row">
+          <div id="liontestimonial" class="owl-carousel owl-theme">
+            <div class="item testimonials">
+              <img src="/static/img/testimonials/1.jpg">
+              <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <p class="quote"><span>Lorem Ipsum</span> - Excepteur sint occaecat</p>
+              </blockquote>
+            </div>
+            <div class="item testimonials">
+              <img src="/static/img/testimonials/2.jpg">
+              <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <p class="quote"><span>Lorem Ipsum</span> - Excepteur sint occaecat</p>
+              </blockquote>
+            </div>
+            <div class="item testimonials">
+              <img src="/static/img/testimonials/3.jpg">
+              <blockquote>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                <p class="quote"><span>Lorem Ipsum</span> - Excepteur sint occaecat</p>
+              </blockquote>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row text-center">
+        <div class="col-md-12 btn-email">
+          <a class="btn lowercase">javier@poremski.se</a>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
---
+++ src/components/Portfolio.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="content-blocks portfolio">
+    <section class="content">
+      <div class="block-content">
+        <h3 class="block-title">Portfolio</h3>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="filters">
+              <span>Filters :</span>
+              <ul id="filters">
+                <li class="active" data-filter="*">All</li>
+                <li data-filter=".mobile">Mobile Development</li>
+                <li data-filter=".webdesign">Web Design</li>
+                <li data-filter=".tools">Tools</li>
+              </ul>
+            </div>
+            <div class="portfolio-container row isotope" id="portfolio-container">
+
+              <div class="col-md-4 col-sm-6 col-xs-12 portfolio-item webdesign">
+                <a class="open-project" href="project-1.html">
+                  <div class="portfolio-column">
+                    <div class="portfolio-hover">
+                      <div class="portfolio-content">
+                        <h2>Verboflex Web</h2>
+                        <hr>
+                        <p>View details.</p>
+                      </div>
+                      <div class="portfolio-overlay"></div>
+                    </div>
+                    <img src="images/portfolio/masonry/001.jpg">
+                  </div>
+                </a>
+              </div>
+
+              <div class="col-md-4 col-sm-6 col-xs-12 portfolio-item mobile">
+                <a class="open-project" href="project-1.html">
+                  <div class="portfolio-column">
+                    <div class="portfolio-hover">
+                      <div class="portfolio-content">
+                        <h2>Verboflex Mobile</h2>
+                        <hr>
+                        <p>View details.</p>
+                      </div>
+                      <div class="portfolio-overlay"></div>
+                    </div>
+                    <img src="images/portfolio/masonry/001.jpg">
+                  </div>
+                </a>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row text-center">
+        <div class="col-md-12 btn-email">
+          <a class="btn lowercase">javier@poremski.se</a>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
