c997341eede771ac06fd0c8eccaabfe9bdf54192
Javier Poremski <javier@poremski.se>
2017-12-16 17:39:13 +0100

    Update i18n for submessage och fix pluralization

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,7 +4,7 @@
       <div class="block-content">
         <h3 class="block-title">{{ $t('section.about.title.profile') }}</h3>
         <h3 class="subheading" v-html="$t('section.about.subheading')"></h3>
-        <p></p>
+        <p v-html="$t('section.about.submessage')"></p>
         <div class="info-list row">
           <div class="col-sm-6" v-html="$t('section.about.name')"></div>
           <div class="col-sm-6" v-html="$t('section.about.date')"></div>
@@ -16,37 +16,37 @@
       </div>
       <div class="block-content">
         <h3 class="block-title">{{ $t('section.about.title.skills') }}</h3>
-        <label class="progress-bar-label">{{ $t('section.about.dev_lang.html_css') }} - <span>{{ $t('section.about.years_experience', {year: 10}) }}</span></label>
+        <label class="progress-bar-label">{{ $t('section.about.dev_lang.html_css') }} - <span>{{ $t('section.about.years_experience', {year: 10}, 10) }}</span></label>
         <div class="progress">
           <div style="width: 99%;" class="progress-bar" role="progressbar" aria-valuenow="99" aria-valuemin="0" aria-valuemax="100">
             <span>99%</span>
           </div>
         </div>
-        <label class="progress-bar-label">{{ $t('section.about.dev_lang.js') }} - <span>{{ $t('section.about.years_experience', {year: 5}) }}</span></label>
+        <label class="progress-bar-label">{{ $t('section.about.dev_lang.js') }} - <span>{{ $t('section.about.years_experience', {year: 5}, 5) }}</span></label>
         <div class="progress">
           <div style="width: 80%;" class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
             <span>99%</span>
           </div>
         </div>
-        <label class="progress-bar-label">{{ $t('section.about.dev_lang.java') }} - <span>{{ $t('section.about.years_experience', {year: 5}) }}</span></label>
+        <label class="progress-bar-label">{{ $t('section.about.dev_lang.java') }} - <span>{{ $t('section.about.years_experience', {year: 5}, 5) }}</span></label>
         <div class="progress">
           <div style="width: 85%;" class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
             <span>99%</span>
           </div>
         </div>
-        <label class="progress-bar-label">{{ $t('section.about.dev_lang.android') }} - <span>{{ $t('section.about.years_experience', {year: 5}) }}</span></label>
+        <label class="progress-bar-label">{{ $t('section.about.dev_lang.android') }} - <span>{{ $t('section.about.years_experience', {year: 5}, 5) }}</span></label>
         <div class="progress">
           <div style="width: 45%;" class="progress-bar" role="progressbar" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100">
             <span>99%</span>
           </div>
         </div>
-        <label class="progress-bar-label">{{ $t('section.about.dev_lang.python') }} - <span>{{ $t('section.about.years_experience', {year: 5}) }}</span></label>
+        <label class="progress-bar-label">{{ $t('section.about.dev_lang.python') }} - <span>{{ $t('section.about.years_experience', {year: 5}, 5) }}</span></label>
         <div class="progress">
           <div style="width: 90%;" class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
             <span>99%</span>
           </div>
         </div>
-        <label class="progress-bar-label">{{ $t('section.about.dev_lang.swift') }} - <span>{{ $t('section.about.years_experience', {year: 2}) }}</span></label>
+        <label class="progress-bar-label">{{ $t('section.about.dev_lang.swift') }} - <span>{{ $t('section.about.years_experience', {year: 2}, 2) }}</span></label>
         <div class="progress">
           <div style="width: 40%;" class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100">
             <span>99%</span>
